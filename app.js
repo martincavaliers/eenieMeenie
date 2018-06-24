9215626e7390cfe5812d307e00b19195d41ea956
martincavaliers <martincoutts5@gmail.com>
2018-06-24 22:58:04 +0100

    For loop working which pushes inpu values to array

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 'use strict';
 
 const addChoiceBtn = document.getElementById('addChoiceBtn');
+const removeChoiceBtn = document.getElementById('removeChoiceBtn');
 const startBtn = document.getElementById('startBtn');
 const inputDiv = document.getElementById('inputDiv');
+let inputCount = inputDiv.childElementCount;
 
+let choiceArray = new Array();
+
+// Creates a new input element then sets it's attributes
 function addChoice(){
     const choice = inputDiv.childElementCount + 1;
     const newInput = document.createElement("INPUT");
@@ -12,10 +17,22 @@ function addChoice(){
     document.getElementById('inputDiv').appendChild(newInput);
 }
 
+//Event listener on Add Choice Button
 addChoiceBtn.addEventListener('click', (e) => {
     addChoice();
 });
 
+// Event listener on Remove Choice button
+    // Removes last child of input div
 removeChoiceBtn.addEventListener('click', (e) => {
     inputDiv.removeChild(inputDiv.lastChild);
+});
+
+// // Event listener on Start button
+startBtn.addEventListener('click', (e) => {
+    for(let i = 0; i < inputCount; i++){
+        let inputs = document.querySelectorAll('input');
+        let x = inputs[i].value;
+        choiceArray.push(x);
+    }
 });
